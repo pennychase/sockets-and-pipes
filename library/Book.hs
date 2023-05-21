c956121d807c9076326ff8ceede637c5e4b7b4b0
Penny Chase <mpchase@mac.com>
2023-05-21 13:24:48 -0400

    Chapter 1 exercises

--- library/Book.hs
+++ library/Book.hs
@@ -1,6 +1,7 @@
 module Book where
 
 import Control.Exception.Safe (tryAny)
+import Control.Monad.Trans.Resource (ReleaseKey, ResourceT, allocate, runResourceT)
 import Relude
 import qualified System.Directory as Dir
 import System.FilePath ((</>))
@@ -36,3 +37,64 @@ writeGreetingTry = do
             IO.hPutStrLn h "world"
             IO.hClose h
             IO.hPutStrLn IO.stderr "Done :)"
+
+-- Ensure file handles are closed using 
+writeGreetingSafe = runResourceT @IO do
+    dir <- liftIO getDataDir
+    (_releaseKey, h) <- allocate (IO.openFile (dir </> "greeting.txt") WriteMode) IO.hClose
+    liftIO (IO.hPutStrLn h "hello")
+    liftIO (IO.hPutStrLn h "world")
+
+--
+-- Chapter 1 Exercises
+--
+
+-- Exercise 1.1 - File resource function
+
+fileResource :: FilePath -> IOMode -> ResourceT IO (ReleaseKey, Handle)
+fileResource path mode = do
+    allocate (IO.openFile path mode) IO.hClose
+
+writeGreetingSafe' = runResourceT @IO do
+    dir <- liftIO getDataDir
+    (_releaseKey, h) <- fileResource (dir </> "greeting.txt") WriteMode
+    liftIO (IO.hPutStrLn h "hello")
+    liftIO (IO.hPutStrLn h "world")
+
+-- Exercise 1.2 - Showing handles
+
+handlePrintTest :: IO ()
+handlePrintTest = do
+    dir <- getDataDir
+    h <- IO.openFile (dir </> "greeting.txt") ReadMode
+    putStrLn $ show h
+    IO.hShow h >>= putStrLn
+
+-- Exercise 1.3 - Exhaustion
+
+howManyHandles = runResourceT @IO do
+    hs <- openManyHandles 
+    putStrLn ("Opened " <> show (length hs) <> " handles")
+
+openManyHandles :: ResourceT IO [Handle]
+openManyHandles = do
+    go []
+    where 
+        go hs = do
+            result <- fileResourceMaybe
+            case result of
+                Just h -> go (h:hs)
+                Nothing -> return hs
+
+fileResourceMaybe :: ResourceT IO (Maybe Handle)
+fileResourceMaybe = do
+    dir <- liftIO getDataDir
+    result <- tryAny $ allocate (IO.openFile (dir </> "greeting.txt") ReadMode) IO.hClose
+    case result of
+        Right x -> return $ Just (snd x)
+        Left e -> do
+            print (displayException e)
+            return Nothing
+
+
+
